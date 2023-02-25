182048894602cd286e89a188af79671af70f3673
isomorpheric <ecam900@gmail.com>
2023-02-25 02:52:31 -0400

    fix: if compile core before importing it works

--- apps/server/src/functions/index.ts
+++ apps/server/src/functions/index.ts
@@ -1,12 +1,12 @@
 import * as functions from 'firebase-functions';
 
-// import { writeHello } from 'core'; // 👈 UNCOMMENT ME!
+import { writeHello } from 'core'; // 👈 UNCOMMENT ME!
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info('Hello logs!', { structuredData: true });
 
-  // const greeting = writeHello(); // 👈 UNCOMMENT ME!
+  const greeting = writeHello(); // 👈 UNCOMMENT ME!
 
-  const greeting = 'IDK feeling loggy'; // 🕶️ MAKE ME A COMMENT
+  // const greeting = 'IDK feeling loggy'; // 🕶️ MAKE ME A COMMENT
   response.send(greeting);
 });
